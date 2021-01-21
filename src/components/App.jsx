7d6f7a2844633e75c97325407853648d73b5f7b6
Gaurav Anand <gaurav9@vt.edu>
2021-01-20 20:40:14 -0500

    Revert "Updated App to reflect changes in data.js"
    This reverts commit 5f084097462c22735a037776cb02605d9ee1e29f.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,17 @@ import React, { useState, useEffect } from 'react';
 import Hero from './Hero/Hero';
 import About from './About/About';
 import Projects from './Projects/Projects';
-import Projects1 from './Projects/Projects1';
 import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
 
 import { PortfolioProvider } from '../context/context';
 
-import { heroData, aboutData, projectsData, projectsData1, contactData, footerData } from '../mock/data';
+import { heroData, aboutData, projectsData, contactData, footerData } from '../mock/data';
 
 function App() {
   const [hero, setHero] = useState({});
   const [about, setAbout] = useState({});
   const [projects, setProjects] = useState([]);
-  const [projects1, setProjects1] = useState([]);
   const [contact, setContact] = useState({});
   const [footer, setFooter] = useState({});
 
@@ -22,17 +20,15 @@ function App() {
     setHero({ ...heroData });
     setAbout({ ...aboutData });
     setProjects([...projectsData]);
-    setProjects1([...projectsData1]
     setContact({ ...contactData });
     setFooter({ ...footerData });
   }, []);
 
   return (
-    <PortfolioProvider value={{ hero, about, projects, projects1, contact, footer }}>
+    <PortfolioProvider value={{ hero, about, projects, contact, footer }}>
       <Hero />
       <About />
       <Projects />
-      <Projects1 /> 
       <Contact />
       <Footer />
     </PortfolioProvider>
