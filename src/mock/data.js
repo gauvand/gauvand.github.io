85d23e70526816a0b1aea85371dd063d2ce42b8b
Gaurav Anand <gaurav9@vt.edu>
2021-01-23 19:59:11 -0500

    added extra section in mock.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -53,6 +53,15 @@ export const projectsData = [
     url: '',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  }
 ];
 
 // CONTACT DATA
