5c6bf3e4d5580cc14dea1289e977755b3b386d8f
Gaurav Anand <gauravanand@Gauravs-MacBook-Air.local>
2021-02-13 18:25:50 -0500

    Revert "Added GTM ID"
    This reverts commit fd8d90afeab7ec51727df1a3cb63ce60f1fc526a.

--- src/html.js
+++ src/html.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 import TagManager from 'react-gtm-module'
  
 const tagManagerArgs = {
-    gtmId: 'G-1W6Z1017QM'
+    gtmId: 'GTM-000000'
 }
  
 TagManager.initialize(tagManagerArgs)
